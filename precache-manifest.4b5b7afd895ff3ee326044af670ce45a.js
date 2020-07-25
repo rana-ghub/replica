444d2373b54c1e95e02a09a55711917d3b57e9ee
rana-ghub <ssshashankrana001@gmail.com>
2020-07-25 16:03:32 +0530

    Updates

---
+++ precache-manifest.4b5b7afd895ff3ee326044af670ce45a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5048f57a9f50c7e8dc06203ceb192e24",
+    "url": "/replica/index.html"
+  },
+  {
+    "revision": "d9110c58493329039701",
+    "url": "/replica/static/css/main.173867ce.chunk.css"
+  },
+  {
+    "revision": "907a3773f16afdf2a657",
+    "url": "/replica/static/js/2.6ee31b0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/replica/static/js/2.6ee31b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9110c58493329039701",
+    "url": "/replica/static/js/main.6f09d3ab.chunk.js"
+  },
+  {
+    "revision": "fb917a4e43e8eec1acff",
+    "url": "/replica/static/js/runtime-main.a3291a98.js"
+  },
+  {
+    "revision": "fbe2e14bb4b82a43caaf441337d972f4",
+    "url": "/replica/static/media/capture.fbe2e14b.jpg"
+  },
+  {
+    "revision": "67b2c1151c9636651dea856b68c4fc7e",
+    "url": "/replica/static/media/car1.67b2c115.jpg"
+  },
+  {
+    "revision": "7fe7e2a2908f8bff57fd117ec5dfa389",
+    "url": "/replica/static/media/car_2.7fe7e2a2.jpg"
+  },
+  {
+    "revision": "fb91f2d2ab033fb8b1b702943f3d937b",
+    "url": "/replica/static/media/car_3.fb91f2d2.jpg"
+  },
+  {
+    "revision": "e7012b5b8827bfef66aa38ef46f6e3e9",
+    "url": "/replica/static/media/car_4.e7012b5b.jpg"
+  },
+  {
+    "revision": "645957f55307070f7eb4e317a77e8f54",
+    "url": "/replica/static/media/car_5.645957f5.jpg"
+  },
+  {
+    "revision": "ae3e9dc044031e11b452418603128ab5",
+    "url": "/replica/static/media/car_6.ae3e9dc0.jpg"
+  },
+  {
+    "revision": "631e2de408107df8a69f2e9a90b13b5a",
+    "url": "/replica/static/media/garden.631e2de4.jpg"
+  },
+  {
+    "revision": "d6c1128aeab5adf60c04bebb85e064d6",
+    "url": "/replica/static/media/map.d6c1128a.jpg"
+  },
+  {
+    "revision": "0341c09a7b6e1f62a5aa6f5d50de3d93",
+    "url": "/replica/static/media/room.0341c09a.jpg"
+  }
+]);
